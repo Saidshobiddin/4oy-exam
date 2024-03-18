9356ffcb0c626f1e4793acc031fc28ce1ffac73a
unknown <saidshohobiddinmansurov75@gmail.com>
2024-03-18 23:17:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+//  let c = +prompt("Number kiriting");
+
+// let e = c/2
+// alert(e);
+
+
+// a = alert(e);
+// alert(b);
+
+// alert(Math.floor(a));
+
+
+
+
+
+// let a =(12.510)
+// let b = Math.trunc(a)
+// alert(b);
+
+
+// let a = "Mars IT school"
+// let b = a.length
+// alert(b)
+
+
+// let a =["Mars IT school"]
+// let b = confirm(a)
+// if (b == true){
+//     a.push("Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school")
+// }
+// else if(b == false ){
+//     a.pop("Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school","Mars IT school")
+// }
+
+// console.log(a);
+
+//   let c = ['a','b','c','d','e','f','g','h','i','j','k','l','o','m','o','p','q','r','s','t','u','v','w','x','y','z','n']
+//  let x = c[18]+c[0]+c[8]+c[3]+c[18]+c[7]+c[0]+c[7]+c[12]+c[1]+c[8]+c[3]+c[3]+c[8]+c[26]
+//  console.log(x);
+
+
+// let a =prompt("yoshiz nechida")
+// if(a== 18){
+//     alert("Siz balogat yoshga yetkansiz")
+// }
+// else if ( a== 17){
+//     alert("Siz balogat yoshga yetmagansiz")
+// }
+// else if ( a== 19){
+//     alert("Siz balogat yoshga yetkansiz")
+// }
+
+// alert(a)
+
+
+
+// let a =prompt('ismizzi kiriting');
+// let reverse = a.split('').reverse().join('');
+// alert(reverse);
+
+
+
+let a =Math.round(Math.random() * 5+5 )
+alert(a);
+if( a == 2,4,6,8,10){
+    alert("bu son juft sonlar")
+}
+else {
+    alert("bu  juft son mas")
+}
